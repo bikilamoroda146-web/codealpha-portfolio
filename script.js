3fc90b4473530704a888760f6b837ed4cc3bb98d
Bim Nek <bikilamoroda146@gmail.com>
2026-04-17 20:43:11 +0300

    insert project with image

--- script.js
+++ script.js
@@ -4,32 +4,24 @@ function toggleMenu() {
 }
 function toggleTheme() {
   document.body.classList.toggle("light");
-
   const icon = document.getElementById("themeIcon");
-
-  if (document.body.classList.contains("light")) {
-    icon.classList.remove("fa-moon");
-    icon.classList.add("fa-sun");
-  } else {
-    icon.classList.remove("fa-sun");
-    icon.classList.add("fa-moon");
-  }
+  icon.classList.toggle("fa-moon");
+  icon.classList.toggle("fa-sun");
 }
 const words = [
   { text: "Frontend Developer", color: "#da1a44" },
-  { text: "React Developer", color: "hsl(0, 0%, 100%)" },
+  { text: "React Developer", color: "#ffffff" },
   { text: "Fullstack Learner", color: "#f7cb0b" }
 ];
-
 let i = 0;
 let j = 0;
 let isDeleting = false;
 function type() {
   const typed = document.getElementById("typed-text");
   const current = words[i].text;
-  const color = words[i].color;
-  typed.style.color = color;
+  typed.style.color = words[i].color;
   typed.textContent = current.substring(0, j);
+
   if (!isDeleting) {
     j++;
     if (j > current.length) {
@@ -47,61 +39,35 @@ function type() {
   setTimeout(type, isDeleting ? 60 : 120);
 }
 type();
-function revealOnScroll() {
-  reveals.forEach((el) => {
-    const elementTop = el.getBoundingClientRect().top;
-    const windowHeight = window.innerHeight;
-    if (elementTop < windowHeight - 100) {
-      el.classList.add("active");
-    }
-  });
-}
-window.addEventListener("scroll", revealOnScroll);
-window.addEventListener("scroll", () => {
-  const scrollTop = document.documentElement.scrollTop;
-  const height =
-    document.documentElement.scrollHeight -
-    document.documentElement.clientHeight;
-  const progress = (scrollTop / height) * 100;
-  document.getElementById("progress-bar").style.width = progress + "%";
-});
-window.addEventListener("load", () => {
-  revealOnScroll();
-});
+
 (function () {
   emailjs.init("QWbMyj7RDkKsp9vPB");
 })();
-document.getElementById("contact-form").addEventListener("submit", function (e) {
+const form = document.getElementById("contact-form");
+const btn = document.getElementById("sendBtn");
+form.addEventListener("submit", function (e) {
   e.preventDefault();
+  btn.innerText = "Sending...";
+  btn.disabled = true;
   emailjs.sendForm(
     "service_dp82use",
     "template_6xl67bo",
     this
   )
-.then(() => {
-  document.getElementById("popup").style.display = "flex";
-  this.reset();
-})
+  .then(() => {
+    document.getElementById("popup").style.display = "flex";
+    form.reset();
+  })
   .catch((err) => {
     alert("Failed to send message");
     console.log(err);
+  })
+  .finally(() => {
+    btn.innerText = "Send Message";
+    btn.disabled = false;
   });
 });
+ 
 function closePopup() {
   document.getElementById("popup").style.display = "none";
 }
-const btn = document.getElementById("sendBtn");
-document.getElementById("contact-form").addEventListener("submit", function (e) {
-  e.preventDefault();
-  btn.innerText = "Sending...";
-  btn.disabled = true;
-  emailjs.sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", this)
-    .then(() => {
-      document.getElementById("popup").style.display = "flex";
-      this.reset();
-    })
-    .finally(() => {
-      btn.innerText = "Send Message";
-      btn.disabled = false;
-    });
-});
